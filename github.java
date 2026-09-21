4b8af56921f25f882fd212d7b6ed78223b141cc7
philoiweko007-del <philo.iweko007@gmail.com>
2026-09-21 08:53:05 +0100

    Create github.java

---
+++ github.java
@@ -0,0 +1,11 @@
+public class github {
+    public static void main(String[] args) {
+        double amount = 500;
+
+        double total = amount + 800;
+        System.out.println("original amount" + amount);
+
+        if (amount > 600) ;
+        System.out.println("Total payment" + amount);
+    }
+}
